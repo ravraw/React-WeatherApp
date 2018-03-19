ab6b800005495c8edd7e2280d9e62a6f82531d59
ravindra rawat <ravinrawat.wd@gmail.com>
2018-03-19 17:09:32 -0600

    app.js updated

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+class App extends React.Component {
+    render(){
+    return (
+        <div>
+            <h1>hello!</h1>
+            <h2>world!</h2>
+        </div>
+        )
+    };
+}    
+
+export default App;
